7cf445fe94e85f4d5f9c81e901d27ef8e880f73e
Prajwal g <98144541+prajwalg0816@users.noreply.github.com>
2026-01-16 15:37:41 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+const supabase = supabaseJs.createClient(
+  "https://nebcrztyezktxbqghxkl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lYmNyenR5ZXprdHhicWdoeGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NTUyMzYsImV4cCI6MjA4NDEzMTIzNn0.Keij0FGmik3yaAAM6VYv2DPBGYdwIzZyKnqxkTaIKAk"
+);
+</script>
